6cbac623caf72aaf01bf4394d16baf0bb7880039
Alex Ryabtsev <alex.ryabtsev@cgi.com>
2018-08-29 15:43:23 -0400

    Refactor code for more generic to accomodate Load Balancers as asset

--- union.py
+++ union.py
@@ -4,18 +4,21 @@ import re
 import uuid
 import time
 import random
+import socket
 from datetime import datetime
 
 print('Loading function ' + datetime.now().time().isoformat())
 route53 = boto3.client('route53')
 ec2 = boto3.resource('ec2')
 compute = boto3.client('ec2')
+elb = boto3.client('elb')
+elbv2 = boto3.client('elbv2')
 dynamodb_client = boto3.client('dynamodb')
 dynamodb_resource = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     """ Check to see whether a DynamoDB table already exists.  If not, create it.  This table is used to keep a record of
-    instances that have been created along with their attributes.  This is necessary because when you terminate an instance
+    assets that have been created along with their attributes.  This is necessary because when you terminate it
     its attributes are no longer available, so they have to be fetched from the table."""
     tables = dynamodb_client.list_tables()
     if 'DDNS' in tables['TableNames']:
@@ -24,75 +27,58 @@ def lambda_handler(event, context):
         create_table('DDNS')
 
     # Set variables
-    # Get the state from the Event stream
-    state = event['detail']['state']
-
-    # Get the instance id, region, and tag collection
-    instance_id = event['detail']['instance-id']
-    region = event['region']
     table = dynamodb_resource.Table('DDNS')
 
-    if state == 'running':
-        time.sleep(60)
-        instance = compute.describe_instances(InstanceIds=[instance_id])
-        # Remove response metadata from the response
-        instance.pop('ResponseMetadata')
-        # Remove null values from the response.  You cannot save a dict/JSON document in DynamoDB if it contains null
-        # values
-        instance = remove_empty_from_dict(instance)
-        instance_dump = json.dumps(instance,default=json_serial)
-        instance_attributes = json.loads(instance_dump)
-        table.put_item(
-            Item={
-                'InstanceId': instance_id,
-                'InstanceAttributes': instance_attributes
-            }
-        )
+    # Check actual event type
+    # And get the asset id, region, and tag collection
+    if event['source'] == "aws.ec2" then:
+      set_instance_vars()
+    elif event['source'] == "aws.elasticloadbalancing":
+      try:
+        assetname = event['detail']['requestParameters']['loadBalancerName']
+        set_lbv1_vars()
+      except:
+        set_lbv2_vars()
     else:
-        # Fetch item from DynamoDB
-        instance = table.get_item(
-        Key={
-            'InstanceId': instance_id
-        },
-        AttributesToGet=[
-            'InstanceAttributes'
-            ]
-        )
-        instance = instance['Item']['InstanceAttributes']
+      print 'Unexpected event source %s' % event['source']
+      return
 
-    try:
-        tags = instance['Reservations'][0]['Instances'][0]['Tags']
-    except:
-        tags = []
-    # Get instance attributes
-    private_ip = instance['Reservations'][0]['Instances'][0]['PrivateIpAddress']
-    private_dns_name = instance['Reservations'][0]['Instances'][0]['PrivateDnsName']
-    private_host_name = private_dns_name.split('.')[0]
-    try:
-        public_ip = instance['Reservations'][0]['Instances'][0]['PublicIpAddress']
-        public_dns_name = instance['Reservations'][0]['Instances'][0]['PublicDnsName']
-        public_host_name = public_dns_name.split('.')[0]
-    except BaseException as e:
-        print 'Instance has no public IP or host name', e
+    if asset['extras']['type'] == 'instance':
+      # Get instance attributes
+      private_ip = asset['extras']['private_ip']
+      try:
+          public_ip = asset['extras']['public_ip']
+      except BaseException as e:
+          print 'Instance has no public IP', e
 
-    # Get the subnet mask of the instance
-    subnet_id = instance['Reservations'][0]['Instances'][0]['SubnetId']
-    subnet = ec2.Subnet(subnet_id)
-    cidr_block = subnet.cidr_block
-    subnet_mask = int(cidr_block.split('/')[-1])
+      # Get the subnet mask of the instance
+      subnet = ec2.Subnet(asset['extras']['subnet_id'])
+      cidr_block = subnet.cidr_block
+      subnet_mask = int(cidr_block.split('/')[-1])
 
-    reversed_ip_address = reverse_list(private_ip)
-    reversed_domain_prefix = get_reversed_domain_prefix(subnet_mask, private_ip)
-    reversed_domain_prefix = reverse_list(reversed_domain_prefix)
+      reversed_ip_address = reverse_list(private_ip)
+      reversed_domain_prefix = get_reversed_domain_prefix(subnet_mask, private_ip)
+      reversed_domain_prefix = reverse_list(reversed_domain_prefix)
 
-    # Set the reverse lookup zone
-    reversed_lookup_zone = reversed_domain_prefix + 'in-addr.arpa.'
-    print 'The reverse lookup zone for this instance is:', reversed_lookup_zone
+      # Set the reverse lookup zone
+      reversed_lookup_zone = reversed_domain_prefix + 'in-addr.arpa.'
+      print 'The reverse lookup zone for this instance is:', reversed_lookup_zone
 
     # Get VPC id
-    vpc_id = instance['Reservations'][0]['Instances'][0]['VpcId']
+    vpc_id = asset['extras']['vpc_id'])
     vpc = ec2.Vpc(vpc_id)
-
+    # Get private and public DNS names
+    try:
+      private_dns_name = asset['extras']['private_dns_name']
+      private_host_name = private_dns_name.split('.')[0]
+    except BaseException as e:
+        print 'Asset has no private DNS host name', e
+    try:
+      public_dns_name = asset['extras']['public_dns_name']
+      public_host_name = public_dns_name.split('.')[0]
+    except BaseException as e:
+        print 'Asset has no public DNS host name', e
+    
     # Are DNS Hostnames and DNS Support enabled?
     if is_dns_hostnames_enabled(vpc):
         print 'DNS hostnames enabled for %s' % vpc_id
@@ -127,15 +113,15 @@ def lambda_handler(event, context):
     else:
         print 'No matching reverse lookup zone'
         # create private hosted zone for reverse lookups
-        if state == 'running':
-            create_reverse_lookup_zone(instance, reversed_domain_prefix, region)
+        if event_state == 'create':
+            create_reverse_lookup_zone(vpc_id, reversed_domain_prefix, region)
             reverse_lookup_zone_id = get_zone_id(reversed_lookup_zone)
     # Wait a random amount of time.  This is a poor-mans back-off if a lot of instances are launched all at once.
     time.sleep(random.random())
 
     # Loop through the instance's tags, looking for the zone and cname tags.  If either of these tags exist, check
     # to make sure that the name is valid.  If it is and if there's a matching zone in DNS, create A and PTR records.
-    for tag in tags:
+    for tag in asset['tags']:
         if 'ZONE' in tag.get('Key',{}).lstrip().upper():
             if is_valid_hostname(tag.get('Value')):
                 if tag.get('Value').lstrip().lower() in private_hosted_zone_collection:
@@ -143,7 +129,7 @@ def lambda_handler(event, context):
                     private_hosted_zone_name = tag.get('Value').lstrip().lower()
                     private_hosted_zone_id = get_zone_id(private_hosted_zone_name)
                     private_hosted_zone_properties = get_hosted_zone_properties(private_hosted_zone_id)
-                    if state == 'running':
+                    if event_state == 'create':
                         if vpc_id in map(lambda x: x['VPCId'], private_hosted_zone_properties['VPCs']):
                             print 'Private hosted zone %s is associated with VPC %s' % (private_hosted_zone_id, vpc_id)
                         else:
@@ -170,7 +156,7 @@ def lambda_handler(event, context):
                     public_hosted_zone_name = tag.get('Value').lstrip().lower()
                     public_hosted_zone_id = get_zone_id(public_hosted_zone_name)
                     # create A record in public zone
-                    if state =='running':
+                    if event_state =='create':
                         try:
                             create_resource_record(public_hosted_zone_id, public_host_name, public_hosted_zone_name, 'A', public_ip)
                         except BaseException as e:
@@ -198,7 +184,7 @@ def lambda_handler(event, context):
                     if cname_domain_suffix_id == cname_private_hosted_zone_id:
                         if cname.endswith(cname_private_hosted_zone):
                             #create CNAME record in private zone
-                            if state == 'running':
+                            if event_state == 'create':
                                 try:
                                     create_resource_record(cname_private_hosted_zone_id, cname_host_name, cname_private_hosted_zone, 'CNAME', private_dns_name)
                                 except BaseException as e:
@@ -212,7 +198,7 @@ def lambda_handler(event, context):
                     if cname.endswith(cname_public_hosted_zone):
                         cname_public_hosted_zone_id = get_zone_id(cname_public_hosted_zone)
                         #create CNAME record in public zone
-                        if state == 'running':
+                        if event_state == 'create':
                             try:
                                 create_resource_record(cname_public_hosted_zone_id, cname_host_name, cname_public_hosted_zone, 'CNAME', public_dns_name)
                             except BaseException as e:
@@ -241,7 +227,7 @@ def lambda_handler(event, context):
             private_hosted_zone_id = get_zone_id(private_hosted_zone_name)
             private_hosted_zone_properties = get_hosted_zone_properties(private_hosted_zone_id)
             # create A records and PTR records
-            if state == 'running':
+            if event_state == 'create':
                 if vpc_id in map(lambda x: x['VPCId'], private_hosted_zone_properties['VPCs']):
                     print 'Private hosted zone %s is associated with VPC %s' % (private_hosted_zone_id, vpc_id)
                 else:
@@ -266,10 +252,10 @@ def lambda_handler(event, context):
             print 'No matching zone for %s' % configuration[0]
             
     # Clean up DynamoDB after deleting records
-    if state != 'running':
+    if event_state != 'create':
         table.delete_item(
             Key={
-                'InstanceId': instance_id
+                'AssetId': asset_id
             }
         )
         
@@ -278,13 +264,13 @@ def create_table(table_name):
             TableName=table_name,
             AttributeDefinitions=[
                 {
-                    'AttributeName': 'InstanceId',
+                    'AttributeName': 'AssetId',
                     'AttributeType': 'S'
                 },
             ],
             KeySchema=[
                 {
-                    'AttributeName': 'InstanceId',
+                    'AttributeName': 'AssetId',
                     'KeyType': 'HASH'
                 },
             ],
@@ -295,6 +281,137 @@ def create_table(table_name):
         )
     table = dynamodb_resource.Table(table_name)
     table.wait_until_exists()
+
+def set_instance_vars():
+  global asset_id, asset, event_state
+
+  asset_id = event['detail']['instance-id'] 
+
+  if event['detail']['state'] == 'running':
+    time.sleep(60)
+    event_state = 'create'
+    asset = compute.describe_instances(InstanceIds=[asset_id])
+    # Remove response metadata from the response
+    asset.pop('ResponseMetadata')
+    try:
+      tags = asset['Reservations'][0]['Instances'][0]['Tags']
+    except:
+      tags = []
+    asset['tags'] = tags
+    asset['extras'] = {}
+    asset['extras']['type'] = 'instance'
+    asset['extras']['region'] = event['region']
+    asset['extras']['private_ip'] = asset['Reservations'][0]['Instances'][0]['PrivateIpAddress']
+    asset['extras']['private_dns_name'] = asset['Reservations'][0]['Instances'][0]['PrivateDnsName']
+    try:
+      asset['extras']['public_ip'] = asset['Reservations'][0]['Instances'][0]['PublicIpAddress']
+      asset['extras']['public_dns_name'] = asset['Reservations'][0]['Instances'][0]['PublicDnsName']
+    except BaseException as e:
+      print 'Instance has no public IP or host name', e
+    asset['extras']['subnet_id'] = asset['Reservations'][0]['Instances'][0]['SubnetId']
+    asset['extras']['vpc_id'] = asset['Reservations'][0]['Instances'][0]['VpcId']
+    db_put_asset(asset_id, asset, table)
+  else:
+    event_state = 'destroy'
+    # Fetch item from DynamoDB
+    db_fetch_asset(asset_id, asset, table)
+
+def set_lbv1_vars():
+  global asset_id, asset, event_state
+
+  asset_id = event['detail']['requestParameters']['loadBalancerName']
+  
+  if event['detail']['eventName'] == 'CreateLoadBalancer':
+    time.sleep(60)
+    event_state = 'create'
+    asset = elb.describe_load_balancers(LoadBalancerNames=[asset_id])
+    # Remove response metadata from the response
+    asset.pop('ResponseMetadata')
+    try:
+      tags = elb.describe_tags(LoadBalancerNames=[asset_id])['TagDescriptions'][0]['Tags']
+    except:
+      tags = []
+    asset['tags'] = tags
+    asset['extras'] = {}
+    asset['extras']['type'] = 'elb'
+    asset['extras']['version'] = 'v1'
+    # Perhaps 'region' could/should be derived from availability zone
+    asset['extras']['region'] = event['detail']['awsRegion']
+    asset['extras']['lb_scheme'] = asset['LoadBalancerDescriptions'][0]['Scheme']
+    if asset['extras']['lb_scheme'] == 'internal':
+      asset['extras']['private_dns_name'] = asset['LoadBalancerDescriptions'][0]['DNSName']
+    else:
+      asset['extras']['public_dns_name'] = asset['LoadBalancerDescriptions'][0]['DNSName']
+    asset['extras']['vpc_id'] = asset['LoadBalancerDescriptions'][0]['VPCId']
+    db_put_asset(asset_id, asset, table)
+  else:
+    event_state = 'destroy'
+    db_fetch_asset(asset_id, asset, table)
+
+def set_lbv2_vars():
+  global asset_id, asset, event_state
+
+  if event['detail']['eventName'] == 'CreateLoadBalancer':
+    time.sleep(60)
+    event_state='create'
+#    lbv2_name = event['detail']['requestParameters']['name']
+#    asset_id = elbv2.describe_load_balancers(Names=[lbv2_name])['LoadBalancers'][0]['LoadBalancerArn']
+    asset_id = event['detail']['responseElements']['loadBalancers'][0]['loadBalancerArn']
+    asset = elbv2.describe_load_balancers(LoadBalancerArns=[asset_id])
+    asset.pop('ResponseMetadata')
+    try:
+      tags = elbv2.describe_tags(ResourceArns=[asset_id])['TagDescriptions'][0]['Tags']
+    except:
+      tags = []
+    asset['tags'] = tags
+    asset['extras'] = {}
+    asset['extras']['type'] = 'elb'
+    asset['extras']['version'] = 'v2'
+    # Perhaps 'region' could/should be derived from availability zone
+    asset['extras']['region'] = event['detail']['awsRegion']
+    asset['extras']['lb_scheme'] = asset['LoadBalancers'][0]['Scheme']
+    if asset['extras']['lb_scheme'] == 'internal':
+      asset['extras']['private_dns_name'] = asset['LoadBalancers'][0]['DNSName']
+    else:
+      asset['extras']['public_dns_name'] = asset['LoadBalancers'][0]['DNSName']
+    asset['extras']['vpc_id'] = asset['LoadBalancers'][0]['VpcId']
+    db_put_asset(asset_id, asset, table)
+  else:
+    event_state='destroy'
+    asset_id = event['detail']['requestParameters']['loadBalancerArn']
+    db_fetch_asset(asset_id, asset, table)
+
+def db_put_asset(asset_id, asset, table):
+  # Remove null values from the response.  You cannot save a dict/JSON document in DynamoDB if it contains null
+  # values
+  asset = remove_empty_from_dict(asset)
+  asset_dump = json.dumps(asset,default=json_serial)
+  asset_attributes = json.loads(asset_dump)
+
+  table.put_item(
+      Item={
+          'AssetId': asset_id,
+          'AssetAttributes': asset_attributes
+      }
+  )
+
+def db_fetch_asset(asset_id, asset, table):
+  # Fetch item from DynamoDB
+  asset = table.get_item(
+  Key={
+      'InstanceId': asset_id
+  },
+  AttributesToGet=[
+      'InstanceAttributes'
+      ]
+  )
+  asset = asset['Item']['InstanceAttributes']
+  # Make sure that empty elements are initialized
+  try:
+    tags = asset['tags']
+  except:
+    tags = []
+  asset['tags'] = tags
 
 def create_resource_record(zone_id, host_name, hosted_zone_name, type, value):
     """This function creates resource records in the hosted zone passed by the calling function."""
@@ -381,7 +498,7 @@ def get_dhcp_configurations(dhcp_options_id):
     return zone_names
 
 def reverse_list(list):
-    """Reverses the order of the instance's IP address and helps construct the reverse lookup zone name."""
+    """Reverses the order of the asset's IP address and helps construct the reverse lookup zone name."""
     if (re.search('\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}',list)) or (re.search('\d{1,3}.\d{1,3}.\d{1,3}\.',list)) or (re.search('\d{1,3}.\d{1,3}\.',list)) or (re.search('\d{1,3}\.',list)):
         list = str.split(str(list),'.')
         list = filter(None, list)
@@ -406,14 +523,14 @@ def get_reversed_domain_prefix(subnet_mask, private_ip):
         first_octet = re.search('\d{1,3}.', private_ip)
         return first_octet.group(0)
 
-def create_reverse_lookup_zone(instance, reversed_domain_prefix, region):
+def create_reverse_lookup_zone(vpc_id, reversed_domain_prefix, region):
     """Creates the reverse lookup zone."""
     print 'Creating reverse lookup zone %s' % reversed_domain_prefix + 'in.addr.arpa.'
     route53.create_hosted_zone(
         Name = reversed_domain_prefix + 'in-addr.arpa.',
         VPC = {
             'VPCRegion':region,
-            'VPCId': instance['Reservations'][0]['Instances'][0]['VpcId']
+            'VPCId': vpc_id
         },
         CallerReference=str(uuid.uuid1()),
         HostedZoneConfig={
